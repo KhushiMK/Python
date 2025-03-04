cc61bea7d1a237cc0fe67e082bc159344d5ef624
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2025-03-04 09:34:53 +0530

    Delete Day 1/Bandnamegenerator.py

--- Day 1/task/task.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the Band Name Generator.")
-name = input("What's the name of your city you grew up in?")
-pet = input("What's is your pet's name ?")
-
-print("Your band name is :",name+""+pet)
